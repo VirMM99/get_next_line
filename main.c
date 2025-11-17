4312340e9a9f879b4f30e2e58973424c92e11983
Virginia Miranda Miguelez <vimirand@c2r2s6.42madrid.com>
2025-11-17 11:44:46 +0100

    Subida 4 del GNL Obligatoria con normi y todo OK CON BONUS

--- main.c
+++ main.c
@@ -6,29 +6,40 @@
 /*   By: vimirand <vimirand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 12:49:16 by vimirand          #+#    #+#             */
-/*   Updated: 2025/11/14 16:50:56 by vimirand         ###   ########.fr       */
+/*   Updated: 2025/11/17 11:24:34 by vimirand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include <stdio.h>
+// #include "get_next_line.h"
+// #include <stdio.h>
 
-// int	open(const char *path, int flags)
-// ssize_t	read(int fildes, void *buf, size_t nbytes)
+// // int	open(const char *path, int flags)
+// // ssize_t	read(int fildes, void *buf, size_t nbytes)
 
-int	main(void)
-{
-	int		fd;
-	char	*felipes;
+// int	main(void)
+// {
+// 	int		fd;
+// 	int		fd2;
+// 	char	*felipes;
 
-	fd = open("retrograde_mercury", O_RDONLY);
-	while (1)
-	{
-		felipes = get_next_line(fd);
-		if (!felipes)
-			break ;
-		printf("%s\n", felipes);
-		free(felipes);
-	}
-	return (0);
-}
+// 	fd = open("retrograde_mercury", O_RDONLY);
+// 	fd2 = open("retrograde_mercury2", O_RDONLY);
+// 	while (1)
+// 	{
+// 		felipes = get_next_line(fd);
+// 		if (!felipes)
+// 			break ;
+// 		printf("%s", felipes);
+// 		free(felipes);
+// 	}
+// 	printf("\n");
+// 	while (1)
+// 	{
+// 		felipes = get_next_line(fd2);
+// 		if (!felipes)
+// 			break ;
+// 		printf("%s", felipes);
+// 		free(felipes);
+// 	}
+// 	return (0);
+// }
